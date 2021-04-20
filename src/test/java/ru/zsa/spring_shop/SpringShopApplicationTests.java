f10fb2d3262ac6c38dedef43c9492f216917d5ae
PaulFrere <paulfrere91@gmail.com>
2021-04-20 10:34:59 +0700

    Initial commit

---
+++ src/test/java/ru/zsa/spring_shop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zsa.spring_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
